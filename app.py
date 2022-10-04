39143e85ec73202def871d30de34164f99338238
Nixuge <mail@nixuge.me>
2022-10-04 21:43:48 +0000

    App.py update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/bin/python3
+
 # -*- coding: utf-8 -*-
+
 from typing import Optional
 from flask import Flask, send_file, request
 import os
@@ -9,9 +11,10 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 class VARS:
-    endpoint_b = b"http://localhost:5000/get_data/"
+    endpoint_b = b"https://mcdl.nixuge.me/get_data/"
     secret_key = "4k9Mu1AoQHGh0x"
     server_port = 64670
+
 
 
 class Link:
@@ -30,46 +33,91 @@ class Link:
 
         self.has_key = key == VARS.secret_key
 
+
+
     def data_exists(self) -> bool:
+
         return os.path.exists(self.data_path)
+
     
+
     def _create_folders(self) -> None:
+
         if not os.path.exists(self.data_folder):
+
             os.makedirs(self.data_folder)
+
     
+
     def download_file(self) -> None:
+
         self._create_folders()
+
         r = requests.get(self.full_url)
+
         if self.extension == "json": content = Utils.patch_file(r.content)
+
         else: content = r.content
 
+
+
         with open(self.data_path, "wb") as open_file:
+
             open_file.write(content)
 
 
+
+
+
 class Utils:
+
     @staticmethod
+
     def patch_file(file_content: str):
+
         return file_content.replace(b"https://", VARS.endpoint_b)
 
 
+
+
+
 @app.route("/get_data/<path:path>")
+
 def get_data(path: str = ""):
+
     link = Link(path, request.args.get('key'))
 
+
+
     if not link.is_trusted_domain and not link.has_key:
+
         return "Must be from a trusted domain !"
 
+
+
     if not link.data_exists():
+
         link.download_file()
+
+
 
     return send_file(link.data_path)
 
 
+
+
+
 @app.route("/")
+
 def index():
+
     return "Welcome to le super index fifou trop bien. Pr linstant lexe est po la pck g la flemme voila nsm il est sur ma cle usb ca suffit tres bien mtn demerdez vous mais en gros si qqun veut fo recompiler PolyMC et ds le CMakeList.txt changer 'meta.polymc.org' à 'mcdl.nixuge.me/get_data/meta.polymc.org' et apres cbon voila bon chonc"
 
+
+
 if __name__ == "__main__":
+
     http_server = WSGIServer(('', VARS.server_port), app)
+
     http_server.serve_forever()
+
